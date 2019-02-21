6d145c2eec39d29ff97d33a087fbe4f532ddd544
Isaac Vitor <isaacvitor@gmail.com>
2019-02-20 22:34:34 -0300

    new changes

--- tef/sitef/index.js
+++
@@ -1,6 +0,0 @@
-const helper = require('../../util/helper')
-const layout = require('./v001.7d')
-
-exports.parseA0Data = (data) => {
-  const parserData = helper.parseDataToLayoutSection(layout.a0, data)
-}
